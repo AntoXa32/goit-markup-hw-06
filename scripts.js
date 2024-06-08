fd8e476f4de11d7d264b10a29ff7c6b888930615
AntoXa32 <151663096+AntoXa32@users.noreply.github.com>
2024-06-08 10:28:14 +0300

    add js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Отримати елементи модального вікна та кнопки закриття
   const modalBackdrop = document.querySelector('.backdrop');
   const modal = document.querySelector('.modal');
-  const openModalBtn = document.querySelector('.footer-form-btn');
+  const openModalBtn = document.querySelector('.menu-btn');
   const closeModalBtn = document.querySelector('.modal-close-btn');
 
-  // Функція для відкриття модального вікна
   function openModal(event) {
-    event.preventDefault(); // Зупинити дію за замовчуванням (відправку форми)
+    event.preventDefault();
     modalBackdrop.classList.add('is-open');
-    modal.style.transform = 'translate(-50%, -50%) scale(1)';
+    modal.classList.add('is-open');
   }
 
-  // Функція для закриття модального вікна
   function closeModal() {
     modalBackdrop.classList.remove('is-open');
-    modal.style.transform = 'translate(-50%, -50%) scale(0)';
+    modal.classList.remove('is-open');
   }
 
-  // Обробники подій для відкриття та закриття модального вікна
   openModalBtn.addEventListener('click', openModal);
   closeModalBtn.addEventListener('click', closeModal);
 
-  // Закрити модальне вікно при кліку на фон поза вікном
   modalBackdrop.addEventListener('click', function (event) {
     if (event.target === modalBackdrop) {
       closeModal();
